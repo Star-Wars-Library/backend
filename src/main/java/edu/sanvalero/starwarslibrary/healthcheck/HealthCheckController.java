1d8bab23bcf2afb1e9c069aa8d1120556d673b66
A8-PC00\A8-PC00 <ss>
2022-06-21 11:42:29 +0200

    Local environment with mysql, docker-compose and health check

--- src/main/java/edu/sanvalero/starwarslibrary/healthcheck/HealthCheckController.java
+++ src/main/java/edu/sanvalero/starwarslibrary/healthcheck/HealthCheckController.java
@@ -1,18 +1,16 @@
-package edu.sanvalero.starwarslibrary.test;
+package edu.sanvalero.starwarslibrary.healthcheck;
 
-
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")
-public class TestController {
+@RequestMapping("/health-check")
+public class HealthCheckController {
     @GetMapping()
     public ResponseEntity<String> test() {
-        return new ResponseEntity<>("Hola mundo", HttpStatus.OK);
+        return ResponseEntity.ok("Hola mundo");
     }
 
 }
