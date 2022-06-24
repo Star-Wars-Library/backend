537d41cc0b46a962725be356e1c48d0ea2ea9650
A8-PC00\A8-PC00 <ss>
2022-06-24 13:35:19 +0200

    CORS on controllers, postman collection, swagger json

--- src/main/java/edu/sanvalero/starwarslibrary/ships/ShipController.java
+++ src/main/java/edu/sanvalero/starwarslibrary/ships/ShipController.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ShipController {
     private static final String PATH = "/ships";
     private final Logger log = LoggerFactory.getLogger(ShipController.class);
